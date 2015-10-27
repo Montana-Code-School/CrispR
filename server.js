73a39e38555537b125476655ab45497360834522
d228m3286w <d228m3286w@gmail.com>
2015-10-27 09:43:25 -0600

    pre database connection

--- server.js
+++ server.js
@@ -1,20 +1,45 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-var bodyParser = require('body-parser');
-var router = express.Router();
+var express     = require('express');
+var app         = express();
+var path        = require('path');
+var bodyParser  = require('body-parser');
+var router      = express.Router();
 
 
 
-app.use('/', express.static(path.join(__dirname, 'public')));
+// app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.use(express.static('public'));
 
 
-// app.get('/', function(req, res){
-//     res.sendFile('index.html');
-// })
+app.get('/', function(req, res){
+    res.sendFile('index.html');
+});
+app.get('/', function(req, res){
+  res.readFile('index.html')
+});
 
 app.set('port', process.env.PORT || 4000);
 
 var server = app.listen(app.get('port'), function(){ 
 	console.log('Express server listening on port ' + server.address().port)
 });
+
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
+   
+router.get('/', function(req, res) {
+   res.json({ message: 'hooray! welcome to our api!' });   
+});
+
+app.use('/api', router);
+
+
+
+
+
