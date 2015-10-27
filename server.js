134318e89c489be6031e99d916f0d7702f1cfba7
ed42311 <ed42311@gmail.com>
2015-10-27 10:07:57 -0600

    the server returns console.log

--- server.js
+++ server.js
@@ -10,13 +10,29 @@ var axios       = require('axios');
 
 app.use(express.static('public'));
 
-
 app.get('/', function(req, res){
     res.sendFile('index.html');
 });
-app.get('/', function(req, res){
-  res.readFile('index.html')
-});
+
+
+
+
+function fetchRecipes(){
+
+  var apiKey = '6bdf1a878fa347cdf262b7f9b30714c7';
+  var urlKey = 'http://food2fork.com/api/search?key=';
+  var searchField = '&q=shredded%20chicken';
+
+  axios.get(urlKey + apiKey + searchField)
+  .then(function (response) {
+    console.log(response.data.recipes);
+  })
+  .catch(function (response) {
+    console.log(response);
+  });
+}
+
+
 
 app.set('port', process.env.PORT || 4000);
 
@@ -25,14 +41,10 @@ var server = app.listen(app.get('port'), function(){
 });
 
 
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-
-   
 router.get('/', function(req, res) {
    res.json({ message: 'hooray! welcome to our api!' });   
 });
@@ -41,14 +53,5 @@ app.use('/api', router);
 app.use('/api/recipes', fetchRecipes);
 
 
-
-
-axios.get('/user?ID=12345')
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (response) {
-    console.log(response);
-  });
 
 
