bdacc95ae972f17a9e36be962be6951a4c65b33d
d228m3286w <d228m3286w@gmail.com>
2015-10-29 12:27:26 -0600

    signin page and navbar looking good

--- routes/recipes.js
+++ routes/recipes.js
@@ -17,5 +17,27 @@
      console.log(response);
    });
  }
- 
  module.exports = fetchRecipes;
+
+ 
+ ////////////////////////////////////////////////////////////
+
+
+ // function fetchIngredients( ID, req, res){
+ 
+ //    var foodItem = req.params.foodId;
+ //    var apiKey = '6bdf1a878fa347cdf262b7f9b30714c7';
+ //    var urlKey = 'http://food2fork.com/api/get?key=';
+ //    var searchField = '&rId=' + foodId;
+    
+ 
+ //   axios.get(urlKey + apiKey + searchField)
+ //   .then(function (response) {
+ //     res.json(response.data.recipes);
+ //     console.log(response.data.recipes);
+ //   })
+ //   .catch(function (response) {
+ //     console.log(response);
+ //   });
+ // }
+ // module.exports = fetchIngredients;
