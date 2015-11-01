8cbb7aa7309cb62e423ca299d44b4ac6f62672c2
ed42311 <ed42311@gmail.com>
2015-11-01 15:14:22 -0700

    passport authentication is working

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+// *******************************  Require Concepts Block **************** \\
+
 var express        = require('express');
 var app            = express();
 var path           = require('path');
+
 var bodyParser     = require('body-parser');
-var router         = express.Router();
 var axios          = require('axios');
 var mongoose 	   = require('mongoose');
 var passport 	   = require('passport');
@@ -12,24 +14,24 @@ var cookieParser   = require('cookie-parser');
 var bodyParser     = require('body-parser');
 var session        = require('express-session');
 
-// routes
-
+var db             = require('./models/db');
+var blogModel      = require('./models/user');
+var router         = express.Router();
 var recipeRoutes   = require('./routes/recipes')
 
+require('./config/passport')(passport); 
+
+// *******************************  Concepts block end **************** \\
 
 app.use('/', express.static(path.join(__dirname, 'public')));
-
 app.use(express.static('public'));
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
 
 
 app.set('port', process.env.PORT || 4000);
-require('./config/passport')(passport); // pass passport for configuration
-//////////////////////////////////////////////////////////////////////////
-// set up our express application
+
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser.json()); // get information from html forms
@@ -43,23 +45,17 @@ app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
 
-// routes ======================================================================
-require('./routes/userRoutes.js')(app, passport); // load routes & pass in app & fully configed passport
 
-////////////////////////////////////////////////////////////////////////////
+require('./routes/userRoutes')(app, passport); 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 var server = app.listen(app.get('port'), function(){ 
 	console.log('Express server listening on port ' + server.address().port)
 });
 
-
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-
-
 app.use('/api', router);
-
 app.use('/api/recipes/:foodItem', recipeRoutes);
 // app.use('/api/ingredients/:recipeId', recipeRoutes.fetchIngredients);
 
